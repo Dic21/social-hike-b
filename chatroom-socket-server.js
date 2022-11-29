10e5fb61ae4eada180603d74c94884321b187cd3
Rick <tek199425@gmail.com>
2022-11-29 17:20:11 +0800

    socket server init

---
+++ chatroom-socket-server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const port = 4000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+app.use(express.static('public'));
+
+
+io.on("connection", (socket) => {
+  console.log('user connected');
+
+  socket.on("chat message", function(msg){
+    console.log("message:", msg);
+    io.emit("allMessage", msg);
+  })
+
+  socket.on("audioMessage", function(msg) {
+    io.emit("audioMessage", msg);
+  });
+
+
+  socket.on("disconnect", ()=>{
+    console.log('user disconnected');
+  })
+});
+
+httpServer.listen(port, ()=>{
+  console.log('Server running');
+});
+
